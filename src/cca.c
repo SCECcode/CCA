0a4c4ab9010e36e55cfbe3ea0967a45976308cd7
Mei-Hui Su <mei@MeiPro.local>
2018-03-19 10:57:48 -0700

    update with new config from proj-fix update with data src from intensity to hypocenter added GTL just like in CVM-S4.26(cvms5)

--- src/cca.c
+++ src/cca.c
@@ -30,9 +30,13 @@ int cca_init(const char *dir, const char *label) {
 	// Initialize variables.
 	configuration = calloc(1, sizeof(cca_configuration_t));
 	velocity_model = calloc(1, sizeof(cca_model_t));
+        vs30_map = calloc(1, sizeof(cca_vs30_map_config_t));
 
 	// Configuration file location.
 	sprintf(configbuf, "%s/model/%s/data/config", dir, label);
+
+        // Set up model directories.
+        sprintf(vs30_etree_file, "%s/model/ucvm/ucvm.e", dir);
 
 	// Read the configuration file.
 	if (read_configuration(configbuf, configuration) != SUCCESS)
@@ -52,6 +56,11 @@ int cca_init(const char *dir, const char *label) {
 		return FAIL;
 	}
 
+        if (read_vs30_map(vs30_etree_file, vs30_map) != SUCCESS) {
+                print_error("Could not read the Vs30 map data from UCVM.");
+                return FAIL;
+        }
+
 	// We need to convert the point from lat, lon to UTM, let's set it up.
 	if (!(cca_latlon = pj_init_plus("+proj=latlong +datum=WGS84"))) {
 		print_error("Could not set up latitude and longitude projection.");
@@ -61,6 +70,10 @@ int cca_init(const char *dir, const char *label) {
 		print_error("Could not set up UTM projection.");
 		return FAIL;
 	}
+        if (!(cca_aeqd = pj_init_plus(vs30_map->projection))) {
+                print_error("Could not set up AEQD projection.");
+                return FAIL;
+        }
 
 	// In order to simplify our calculations in the query, we want to rotate the box so that the bottom-left
 	// corner is at (0m,0m). Our box's height is total_height_m and total_width_m. We then rotate the
@@ -168,17 +181,21 @@ int cca_query(cca_point_t *points, cca_properties_t *data, int numpoints) {
 			data[i].qs = -1;
 			continue;
 		} else {
-			// Read all the surrounding point properties.
-			read_properties(load_x_coord,     load_y_coord,     load_z_coord,     &(surrounding_points[0]));	// Orgin.
-			read_properties(load_x_coord + 1, load_y_coord,     load_z_coord,     &(surrounding_points[1]));	// Orgin + 1x
-			read_properties(load_x_coord,     load_y_coord + 1, load_z_coord,     &(surrounding_points[2]));	// Orgin + 1y
-			read_properties(load_x_coord + 1, load_y_coord + 1, load_z_coord,     &(surrounding_points[3]));	// Orgin + x + y, forms top plane.
-			read_properties(load_x_coord,     load_y_coord,     load_z_coord - 1, &(surrounding_points[4]));	// Bottom plane origin
-			read_properties(load_x_coord + 1, load_y_coord,     load_z_coord - 1, &(surrounding_points[5]));	// +1x
-			read_properties(load_x_coord,     load_y_coord + 1, load_z_coord - 1, &(surrounding_points[6]));	// +1y
-			read_properties(load_x_coord + 1, load_y_coord + 1, load_z_coord - 1, &(surrounding_points[7]));	// +x +y, forms bottom plane.
+                        if ( points[i].depth < configuration->depth_interval && configuration->gtl == 1) {
+                           get_vs30_based_gtl(&(points[i]), &(data[i]));
+                        } else {
+			    // Read all the surrounding point properties.
+			    read_properties(load_x_coord,     load_y_coord,     load_z_coord,     &(surrounding_points[0]));	// Orgin.
+			    read_properties(load_x_coord + 1, load_y_coord,     load_z_coord,     &(surrounding_points[1]));	// Orgin + 1x
+			    read_properties(load_x_coord,     load_y_coord + 1, load_z_coord,     &(surrounding_points[2]));	// Orgin + 1y
+			    read_properties(load_x_coord + 1, load_y_coord + 1, load_z_coord,     &(surrounding_points[3]));	// Orgin + x + y, forms top plane.
+			    read_properties(load_x_coord,     load_y_coord,     load_z_coord - 1, &(surrounding_points[4]));	// Bottom plane origin
+			    read_properties(load_x_coord + 1, load_y_coord,     load_z_coord - 1, &(surrounding_points[5]));	// +1x
+			    read_properties(load_x_coord,     load_y_coord + 1, load_z_coord - 1, &(surrounding_points[6]));	// +1y
+			    read_properties(load_x_coord + 1, load_y_coord + 1, load_z_coord - 1, &(surrounding_points[7]));	// +x +y, forms bottom plane.
 
-			trilinear_interpolation(x_percent, y_percent, z_percent, surrounding_points, &(data[i]));
+			    trilinear_interpolation(x_percent, y_percent, z_percent, surrounding_points, &(data[i]));
+                       }
 		}
 
 		// Calculate Qp and Qs.
@@ -209,7 +226,6 @@ void read_properties(int x, int y, int z, cca_properties_t *data) {
 	data->rho = -1;
 	data->qp = -1;
 	data->qs = -1;
-
 	float *ptr = NULL;
 	FILE *fp = NULL;
 	int location = z * configuration->nx * configuration->ny + y * configuration->nx + x;
@@ -266,7 +282,7 @@ void trilinear_interpolation(double x_percent, double y_percent, double z_percen
 
 	bilinear_interpolation(x_percent, y_percent, four_points, &temp_array[0]);
 
-	// Now advance the pointer four "cvms5_properties_t" spaces.
+	// Now advance the pointer four "cca_properties_t" spaces.
 	four_points += 4;
 
 	// Another interpolation.
@@ -389,6 +405,10 @@ int read_configuration(char *file, cca_configuration_t *config) {
 			if (strcmp(key, "bottom_right_corner_e") == 0)	config->bottom_right_corner_e = atof(value);
 			if (strcmp(key, "bottom_right_corner_n") == 0)	config->bottom_right_corner_n = atof(value);
 			if (strcmp(key, "depth_interval") == 0)			config->depth_interval = atof(value);
+                        if (strcmp(key, "gtl") == 0) {
+                                if (strcmp(value, "on") == 0) config->gtl = 1;
+                                else config->gtl = 0;
+                        }
 		}
 	}
 
@@ -526,6 +546,201 @@ int try_reading_model(cca_model_t *model) {
 	else
 		return 2;
 }
+
+
+/**
+ * Reads the format of the Vs30 data e-tree. This file location is typically specified
+ * in the configuration file of the model.
+ *
+ * @param filename The e-tree's file location from which to read.
+ * @param map The outputted map configuration structure.
+ */
+int read_vs30_map(char *filename, cca_vs30_map_config_t *map) {
+	char appmeta[512];
+	char *token;
+	int index = 0, retVal = 0;
+	map->vs30_map = etree_open(filename, O_RDONLY, 64, 0, 3);
+	retVal = snprintf(appmeta, sizeof(appmeta), "%s", etree_getappmeta(map->vs30_map));
+
+	if (retVal >= 0 && retVal < 128) {
+		return FAIL;
+	}
+
+	// Now we need to parse the map configuration.
+	index = 0;
+	token = strtok(appmeta, "|");
+
+	while (token != NULL) {
+		switch (index) {
+	    case 0:
+	    	snprintf(map->type, sizeof(map->type), "%s", token);
+	    	break;
+	    case 1:
+	    	snprintf(map->description, sizeof(map->description), "%s", token);
+	    	break;
+	    case 2:
+	    	snprintf(map->author, sizeof(map->author), "%s", token);
+	    	break;
+	    case 3:
+	    	snprintf(map->date, sizeof(map->date), "%s", token);
+	    	break;
+	    case 4:
+	    	sscanf(token, "%lf", &(map->spacing));
+	    	break;
+	    case 5:
+	    	snprintf(map->schema, sizeof(map->schema), "%s", token);
+	    	break;
+	    case 6:
+	    	snprintf(map->projection, sizeof(map->projection), "%s", token);
+	    	break;
+	    case 7:
+	    	sscanf(token, "%lf,%lf,%lf", &(map->origin_point.longitude), &(map->origin_point.latitude),
+	    			&(map->origin_point.depth));
+	    	break;
+	    case 8:
+	    	sscanf(token, "%lf", &(map->rotation));
+	    	break;
+	    case 9:
+	    	sscanf(token, "%lf,%lf,%lf", &(map->x_dimension), &(map->y_dimension), &(map->z_dimension));
+	    	break;
+	    case 10:
+	    	sscanf(token, "%u,%u,%u", &(map->x_ticks), &(map->y_ticks), &(map->z_ticks));
+	    	break;
+	    default:
+	    	fprintf(stderr, "Unexpected metadata. Please check your Vs30 e-tree within UCVM.\n");
+	    	return FAIL;
+	    	break;
+		}
+	    index++;
+	    token = strtok(NULL, "|");
+	}
+
+	return SUCCESS;
+
+}
+
+/**
+ * Given a latitude and longitude in WGS84 co-ordinates, we find the corresponding e-tree octant
+ * in the Vs30 map e-tree and read the value as well as interpolate bilinearly.
+ *
+ * @param longitude The longitude in WGS84 format.
+ * @param latitude The latitude in WGS84 format.
+ * @param map The Vs30 map structure as defined during the initialization procedure.
+ * @return The Vs30 value at that point, or -1 if outside the boundaries.
+ */
+double get_vs30_value(double longitude, double latitude, cca_vs30_map_config_t *map) {
+	// Convert both points to UTM.
+	double longitude_utm_e = longitude * DEG_TO_RAD;
+	double latitude_utm_n = latitude * DEG_TO_RAD;
+	double vs30_long_utm_e = map->origin_point.longitude * DEG_TO_RAD;
+	double vs30_lat_utm_n = map->origin_point.latitude * DEG_TO_RAD;
+	double temp_rotated_point_n = 0.0, temp_rotated_point_e = 0.0;
+	double rotated_point_n = 0.0, rotated_point_e = 0.0;
+	double percent = 0.0;
+	int loc_x = 0, loc_y = 0;
+	etree_addr_t addr;
+	vs30_mpayload_t vs30_payload[4];
+
+	int max_level = ceil(log(map->x_dimension / map->spacing) / log(2.0));
+	etree_tick_t edgetics = (etree_tick_t)1 << (ETREE_MAXLEVEL - max_level);
+	double map_edgesize = map->x_dimension / (double)((etree_tick_t)1<<max_level);
+
+	pj_transform(cca_latlon, cca_aeqd, 1, 1, &longitude_utm_e, &latitude_utm_n, NULL);
+	pj_transform(cca_latlon, cca_aeqd, 1, 1, &vs30_long_utm_e, &vs30_lat_utm_n, NULL);
+
+	// Now that both are in UTM, we can subtract and rotate.
+	temp_rotated_point_e = longitude_utm_e - vs30_long_utm_e;
+	temp_rotated_point_n = latitude_utm_n - vs30_lat_utm_n;
+
+	rotated_point_e = cos_vs30_rotation_angle * temp_rotated_point_e - sin_vs30_rotation_angle * temp_rotated_point_n;
+	rotated_point_n = sin_vs30_rotation_angle * temp_rotated_point_e + cos_vs30_rotation_angle * temp_rotated_point_n;
+
+	// Are we within the box?
+	if (rotated_point_e < 0 || rotated_point_n < 0 || rotated_point_e > map->x_dimension ||
+		rotated_point_n > map->y_dimension) return -1;
+
+	// Get the integer location of the grid point within the map.
+	loc_x = floor(rotated_point_e / map_edgesize);
+	loc_y = floor(rotated_point_n / map_edgesize);
+
+	// We need the four surrounding points for bilinear interpolation.
+	addr.level = ETREE_MAXLEVEL;
+	addr.x = loc_x * edgetics; addr.y = loc_y * edgetics; addr.z = 0;
+    /* Adjust addresses for edges of grid */
+    if (addr.x >= map->x_ticks) addr.x = map->x_ticks - edgetics;
+    if (addr.y >= map->y_ticks) addr.y = map->y_ticks - edgetics;
+	etree_search(map->vs30_map, addr, NULL, "*", &(vs30_payload[0]));
+	addr.x = (loc_x + 1) * edgetics; addr.y = loc_y * edgetics;
+    if (addr.x >= map->x_ticks) addr.x = map->x_ticks - edgetics;
+    if (addr.y >= map->y_ticks) addr.y = map->y_ticks - edgetics;
+	etree_search(map->vs30_map, addr, NULL, "*", &(vs30_payload[1]));
+	addr.x = loc_x * edgetics; addr.y = (loc_y + 1) * edgetics;
+    if (addr.x >= map->x_ticks) addr.x = map->x_ticks - edgetics;
+    if (addr.y >= map->y_ticks) addr.y = map->y_ticks - edgetics;
+	etree_search(map->vs30_map, addr, NULL, "*", &(vs30_payload[2]));
+	addr.x = (loc_x + 1) * edgetics; addr.y = (loc_y + 1) * edgetics;
+    if (addr.x >= map->x_ticks) addr.x = map->x_ticks - edgetics;
+    if (addr.y >= map->y_ticks) addr.y = map->y_ticks - edgetics;
+	etree_search(map->vs30_map, addr, NULL, "*", &(vs30_payload[3]));
+
+	percent = fmod(rotated_point_e / map->spacing, map->spacing) / map->spacing;
+	vs30_payload[0].vs30 = percent * vs30_payload[0].vs30 + (1 - percent) * vs30_payload[1].vs30;
+	vs30_payload[1].vs30 = percent * vs30_payload[2].vs30 + (1 - percent) * vs30_payload[3].vs30;
+	percent = fmod(rotated_point_n / map->spacing, map->spacing) / map->spacing;
+	vs30_payload[0].vs30 = percent * vs30_payload[0].vs30 + (1 - percent) * vs30_payload[1].vs30;
+
+	return vs30_payload[0].vs30;
+}
+
+/**
+ * Gets the GTL value using the Wills and Wald dataset, given a latitude, longitude and depth.
+ *
+ * @param point The point at which to retrieve the property. Note, depth is ignored.
+ * @param data The material properties at the point specified, or -1 if not found.
+ * @return Success or failure.
+ */
+int get_vs30_based_gtl(cca_point_t *point, cca_properties_t *data) {
+	double a = 0.5, b = 0.6, c = 0.5;
+	double percent_z = point->depth / configuration->depth_interval;
+	double f = 0.0, g = 0.0;
+	double vs30 = 0.0, vp30 = 0.0;
+
+	// Double check that we're above the first layer.
+	if (percent_z > 1) return FAIL;
+
+	// Query for the point at depth_interval.
+	cca_point_t *pt = calloc(1, sizeof(cca_point_t));
+	cca_properties_t *dt = calloc(1, sizeof(cca_properties_t));
+
+	pt->latitude = point->latitude;
+	pt->longitude = point->longitude;
+	pt->depth = configuration->depth_interval;
+
+	if (cca_query(pt, dt, 1) != SUCCESS) return FAIL;
+
+	// Now we need the Vs30 data value.
+	vs30 = get_vs30_value(point->longitude, point->latitude, vs30_map);
+
+	if (vs30 == -1) {
+		data->vp = -1;
+		data->vs = -1;
+	} else {
+		// Get the point's material properties within the GTL.
+		f = percent_z + b * (percent_z - pow(percent_z, 2.0f));
+		g = a - a * percent_z + c * (pow(percent_z, 2.0f) + 2.0 * sqrt(percent_z) - 3.0 * percent_z);
+		data->vs = f * dt->vs + g * vs30;
+		vs30 = vs30 / 1000;
+		vp30 = 0.9409 + 2.0947 * vs30 - 0.8206 * pow(vs30, 2.0f) + 0.2683 * pow(vs30, 3.0f) - 0.0251 * pow(vs30, 4.0f);
+		vp30 = vp30 * 1000;
+		data->vp = f * dt->vp + g * vp30;
+	}
+
+	free(pt);
+	free(dt);
+
+	return SUCCESS;
+}
+
 
 // The following functions are for dynamic library mode. If we are compiling
 // a static library, these functions must be disabled to avoid conflicts.
--- src/cca.h
+++ src/cca.h
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <math.h>
 
+#include "etree.h"
 #include "proj_api.h"
 
 // Constants
@@ -60,6 +61,8 @@ typedef struct cca_configuration_t {
 	int utm_zone;
 	/** The model directory */
 	char model_dir[128];
+        /** GTL on or off (1 or 0) */
+        int gtl;
 	/** Number of x points */
 	int nx;
 	/** Number of y points */
@@ -88,6 +91,43 @@ typedef struct cca_configuration_t {
 	double depth_interval;
 } cca_configuration_t;
 
+/** The configuration structure for the Vs30 map. */
+typedef struct cca_vs30_map_config_t {
+	/** Pointer to the e-tree file */
+	etree_t *vs30_map;
+	/** The type of map */
+	char type[20];
+	/** A description of the map */
+	char description[50];
+	/** The map's author */
+	char author[30];
+	/** The date the map was created */
+	char date[10];
+	/** The spacing in meters */
+	double spacing;
+	/** The map's schema */
+	char schema[50];
+	/** The projection string in Proj.4 format */
+	char projection[128];
+	/** The origin point */
+	cca_point_t origin_point;
+	/** The number of degrees the map was rotated around origin */
+	double rotation;
+	/** The X dimension of the map */
+	double x_dimension;
+	/** The Y dimension of the map */
+	double y_dimension;
+	/** The Z dimension of the map */
+	double z_dimension;
+	/** Number of e-tree ticks in the X direction */
+	int x_ticks;
+	/** Number of e-tree ticks in the Y direction */
+	int y_ticks;
+	/** Number of e-tree ticks in the Z direction */
+	int z_ticks;
+} cca_vs30_map_config_t;
+
+
 /** The model structure which points to available portions of the model. */
 typedef struct cca_model_t {
 	/** A pointer to the Vs data either in memory or disk. Null if does not exist. */
@@ -112,6 +152,14 @@ typedef struct cca_model_t {
 	int qs_status;
 } cca_model_t;
 
+/** Contains the Vs30 and surface values from the UCVM map. */
+typedef struct vs30_mpayload_t {
+        /** Surface height in meters */
+        float surf;
+        /** Vs30 data from Wills and Wald */
+        float vs30;
+} vs30_mpayload_t;
+
 // Constants
 /** The version of the model. */
 const char *version_string = "CCA";
@@ -120,11 +168,17 @@ const char *version_string = "CCA";
 /** Set to 1 when the model is ready for query. */
 int is_initialized = 0;
 
+/** Location of the ucvm.e e-tree file. */
+char vs30_etree_file[128];
 /** Location of En-Jui's latest iteration files. */
 char iteration_directory[128];
 
 /** Configuration parameters. */
 cca_configuration_t *configuration;
+
+/** Holds the configuration parameters for the Vs30 map. */
+cca_vs30_map_config_t *vs30_map;
+
 /** Holds pointers to the velocity model data OR indicates it can be read from file. */
 cca_model_t *velocity_model;
 
@@ -132,6 +186,8 @@ cca_model_t *velocity_model;
 projPJ cca_latlon;
 /** Proj.4 UTM projection holder. */
 projPJ cca_utm;
+/** Proj.4 Vs30 map projection holder. */
+projPJ cca_aeqd;
 
 /** The cosine of the rotation angle used to rotate the box and point around the bottom-left corner. */
 double cos_rotation_angle = 0;
@@ -142,6 +198,11 @@ double sin_rotation_angle = 0;
 double total_height_m = 0;
 /** The width of this model's region, in meters. */
 double total_width_m = 0;
+
+/** The cosine of the Vs30 map's rotation. */
+double cos_vs30_rotation_angle = 0;
+/** The sine of the Vs30 map's rotation. */
+double sin_vs30_rotation_angle = 0;
 
 // UCVM API Required Functions
 
@@ -180,6 +241,14 @@ void read_properties(int x, int y, int z, cca_properties_t *data);
 int try_reading_model(cca_model_t *model);
 /** Calculates density from Vs. */
 double calculate_density(double vs);
+
+// GTL related
+/** Retrieves the vs30 value for a given point. */
+int get_vs30_based_gtl(cca_point_t *point, cca_properties_t *data);
+/** Reads the specified Vs30 map from UCVM. */
+int read_vs30_map(char *filename, cca_vs30_map_config_t *map);
+/** Gets the Vs30 value at a point */
+double get_vs30_value(double longitude, double latitude, cca_vs30_map_config_t *map);
 
 // Interpolation Functions
 /** Linearly interpolates two cca_properties_t structures */
