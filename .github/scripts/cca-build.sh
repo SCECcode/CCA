d7145aa8e4994590c04a2ef61ed248efe47a6867
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-05-13 04:42:16 +0000

    fix ci

--- .github/scripts/cca-build.sh
+++ .github/scripts/cca-build.sh
@@ -8,8 +8,9 @@ if [ $tmp == 'Darwin' ]; then
   brew reinstall gcc
 fi
 
-libtoolize
-autoreconf -i
+aclocal -I m4
+autoreconf -i -f
+automake --add-missing --force-missing
 ./configure --prefix=$UCVM_INSTALL_PATH/model/cca --with-etree-libdir=${UCVM_INSTALL_PATH}/lib/euclid3/lib --with-etree-incdir=${UCVM_INSTALL_PATH}/lib/euclid3/include --with-proj-libdir=${UCVM_INSTALL_PATH}/lib/proj/lib --with-proj-incdir=${UCVM_INSTALL_PATH}/lib/proj/include --with-tiff-libdir=${UCVM_INSTALL_PATH}/lib/tiff/lib --with-tiff-incdir=${UCVM_INSTALL_PATH}/lib/tiff/include --with-sqlite-libdir=${UCVM_INSTALL_PATH}/lib/sqlite/lib --with-sqlite-incdir=${UCVM_INSTALL_PATH}/lib/sqlite/include
 make
 make install
